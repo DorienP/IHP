87a7a8c2e7249a5b6a90d77c8403d4d6cd90f751
Initial-D-Cmd <dorien.pine@gmail.com>
2022-07-08 23:31:32 -0700

    blog

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+
+let
+  haskellDeps = ps: with ps; [
+    base
+    lens
+    mtl
+    random
+  ];
+  haskellEnv = haskell.packages.ghc865.ghcWithPackages haskellDeps;
+in mkShell {
+  buildInputs = [
+    haskellEnv
+    haskellPackages.cabal-install
+    gdb
+  ];
+}
